9926dcc71fbf5d70b994a70c982f9e17789a993f
shadrach akaade <akaadesm@gmail.com>
2022-11-15 22:05:59 +0100

    implement sponsors section

---
+++ src/components/sponsors.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import mbtoken from '../assets/MBTOKEN.svg'
+import metamask from '../assets/METAMASK.svg'
+import opensea from '../assets/OPENSEA.svg'
+
+const Sponsors = () => {
+  return (
+    <div className="text-center" id="sponsors">
+      <div className="row">
+        <div className="col-md">
+          <img
+            src={mbtoken}
+            alt="mbtoken logo"
+            className="img-fluid py-2 mw-100"
+          ></img>
+        </div>
+        <div className="col-md">
+          <img
+            src={metamask}
+            alt="metamask logo"
+            className="img-fluid py-2 mw-100"
+          ></img>
+        </div>
+        <div className="col-md">
+          <img
+            src={opensea}
+            alt="opensea logo"
+            className="img-fluid py-2 mw-100"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sponsors
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Herosection from '../components/herosection'
+import Sponsors from '../components/sponsors'
 
 const Home = () => {
    return (
     <>
     <Herosection />
+    <Sponsors />
     </>
    )
 }
